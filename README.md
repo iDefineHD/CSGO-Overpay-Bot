da336c4c2aed823ac54b286277b3141f720a1731
iDefineHD <purpleizlegitx@gmail.com>
2018-02-10 14:20:12 +0000

    Added support for csgo fast alone.
    Added csgofast api, removed steamapi.io. all credit goes to Arze1 for the original bot.

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# CSGO-Overpay-Bot
-A clone of Arze1, just with csgofast and steamapi.io is a paid service now.
+# CSGO-Overpay-bot
+
+### Setup
+If you haven't done so already please install nodejs.
+Open windows start menu and find cmd.exe (command prompt) and open it.
+Then follow these steps:
+```BASH
+  cd {the dir of the folder} //EX: cd C:/users/arze/desktop/CSGO-Overpay-Bot 
+  npm i
+  node bot.js
+```
+Add your username, password, secrets.
+
+### Config elements
+- `username` AND `password` are the credentials of your steam account you will be using for the bot.
+- `botname` What you want the bots profile name to be, leave blank to not change
+- `identitySecret` AND `sharedSecret` You get theese from [Steam Desktop Authenticator](https://github.com/Jessecar96/SteamDesktopAuthenticator/releases/tag/1.0.7.2) and de-crypting the files
+- `adminIDs` An array of steam64 ids to auto-accept trades from
+- `options`
+	- `appid` AppID to get prices from. 730 (CS:GO) is default id. Other ID's are not tested to work, but still may.
+	- `minimumprice` Minium value for their items
+	- `acceptRandomFriendRequests` boolean value, accepts any friend requests sent to the provided bot account.
+	- `priceRefreshInterval` Time (in seconds) to renew pricelist
+	- `confirmationInterval` Time (in seconds) to check mobile trade authentication.
+	- `percentamount` Percent to value users items at. ex: 95% would be 0.95 in config
+	- `minPricePerItem` Minimum price for items for them to be valued. ex: if set to 0.10 any item under 10 cents would be worth nothing
+	- `chatResponse` Chat message handling
+		- `donation` Message to send to user when they donate skins
+		- `junk` Message to send when users items are less then `minimunprice`
+		- `tradeDeclined` Message to send when users items have less value then bots items
+		- `tradeAccepted` Message to send when trade goes through
+		- `newFriend` Message to send to user when they add the bot
+		- `unknownCommand` Message to send to user when they send you a message that is not a command
+		- `adminTrade` Sent to admin when they make a (auto-accepted) trade to the bot NOTE: admins can be configured in `adminIDs`
+		- `commands` Custom simple commands, JSON object in form of "!command"
+: "res"
+
+
+
+
+
+### CONTRIBUTING
+If you want to contribute you must follow these rules
+- Adding elements to config requires you to update README.md
+- You must comment your code sufficently
+- You have to add enough for it to be merged, EX: i will not merge adding one comment
+- Obviously make sure personal details are out of config when makng a PR
+- Leave placeholder commands and responces in
+- Dont add anything to the bot to scam people
+
+
+All credit is to Arze1 for the bot.
---
+++ bot.js
@@ -0,0 +1,161 @@
+/*
+	Initalizing packages
+*/
+const SteamUser = require('steam-user');
+const TradeOfferManager = require('steam-tradeoffer-manager');
+const SteamTotp = require('steam-totp');
+const SteamCommunity = require('steamcommunity');
+const fs = require('fs');
+const request = require('request');
+const config = require('./config.json');
+
+const community = new SteamCommunity();
+const client = new SteamUser();
+const manager = new TradeOfferManager({
+
+    steam: client,
+    domain: 'example.com',
+    language: 'en'
+});
+
+/*
+	Polling Steam and Logging On
+*/
+client.logOn({
+    accountName: config.username,
+    password: config.password,
+    twoFactorCode: SteamTotp.generateAuthCode(config.sharedSecret)
+});
+
+/*
+	Getting prices
+*/
+const priceUrl = 'https://api.csgofast.com/price/all';
+
+function getPriceList() {
+
+    request(priceUrl, (error, response, body) => {
+        if (error || response.statusCode !== 200) return console.log(`Error: ${error} - Status Code: ${response.statusCode}`);
+        fs.writeFile('prices.json', body);
+    });
+}
+
+function priceItemsInOffer(offer) {
+    let offerValue = 0;
+    if (offer) {
+        const prices = require('./prices.json'); //Requiring price file
+        //Loop through offer and get total price
+        for (var x in offer) {
+            prices[offer[x].market_hash_name] >= config.options.minPricePerItem ? offerValue += prices[offer[x].market_hash_name] : null;
+        }
+    }
+    return offerValue;
+}
+
+//Make the first price request
+getPriceList();
+//Auto Refresh price
+setInterval(getPriceList, config.options.priceRefreshInterval * 1000);
+
+/*
+	Friend requests and chat
+*/
+client.on('friendRelationship', (steamID, relationship) => {
+    if (relationship === 2 && config.options.acceptRandomFriendRequests) {
+        client.addFriend(steamID);
+        client.chatMessage(steamID, config.options.chatResponse.newFriend);
+    }
+});
+
+client.on('friendMessage', (steamID, message) => {
+    console.log(config.options.chatResponse.commands[message]);
+    if (config.options.chatResponse.commands[message]) {
+        client.chatMessage(steamID, config.options.chatResponse.commands[message]);
+    } else {
+        client.chatMessage(steamID, config.options.chatResponse.unknownCommand);
+    }
+});
+
+/*
+	Offer handling
+*/
+function isInArray(value, array) {
+    return array.indexOf(value) > -1;
+}
+
+function acceptOffer(offer) {
+    offer.accept((err) => {
+        if (err) console.log(`Unable to accept offer: ${err.message}`);
+        community.checkConfirmations();
+    });
+}
+
+function declineOffer(offer) {
+    offer.decline((err) => {
+        if (err) return console.log(`Unable to decline offer: ${err.message}`);
+    });
+}
+
+manager.on('newOffer', function(offer) {
+    const partnerID = offer.partner.getSteamID64();
+
+    offer.getUserDetails((err, me, them) => {
+        if (err) return console.log(err);
+
+        if (them.escrowDays > 0) {
+            console.log('Trade is in escrow. Declining.');
+            declineOffer(offer);
+        }
+    });
+
+    console.log(`New offer # ${offer.id} from ${partnerID}`);
+
+    if (isInArray(partnerID, config.adminIDs)) {
+        client.chatMessage(partnerID, config.options.chatResponse.adminTrade);
+        acceptOffer(offer);
+
+    } else if (!offer.itemsToGive.length) {
+        console.log(`${partnerID} just donated us items.`);
+
+        client.chatMessage(partnerID, config.options.chatResponse.donation); //Sending message for donations
+        acceptOffer(offer);
+
+    } else if (priceItemsInOffer(offer.itemsToReceive) < config.options.minimumprice) {
+        client.chatMessage(partnerID, config.options.chatResponse.junk); //Sending message for donations
+        declineOffer(offer);
+
+    } else if (priceItemsInOffer(offer.itemsToGive) > priceItemsInOffer(offer.itemsToReceive) * config.options.percentamount) {
+        client.chatMessage(partnerID, config.options.chatResponse.tradeDeclined); //Sending message when trade declined
+        declineOffer(offer);
+    } else {
+        client.chatMessage(partnerID, config.options.chatResponse.tradeAccepted); //Sending message for accepting offer
+        acceptOffer(offer);
+    }
+});
+
+
+
+//Refresh polldata.json
+manager.on('pollData', function(pollData) {
+    fs.writeFile('polldata.json', JSON.stringify(pollData));
+});
+
+if (fs.existsSync('polldata.json')) {
+    manager.pollData = JSON.parse(fs.readFileSync('polldata.json'));
+}
+
+client.on('loggedOn', function(details) {
+    console.log(`Logged into Steam as ${client.steamID.getSteam3RenderedID()}`);
+    client.setPersona(SteamUser.Steam.EPersonaState.Online, config.botname);
+    client.gamesPlayed([730])
+});
+
+client.on('webSession', function(sessionID, cookies) {
+    manager.setCookies(cookies, function(err) {
+        if (err) return console.log(err);
+        console.log(`Got API key: ${manager.apiKey}`);
+    });
+
+    community.setCookies(cookies);
+    community.startConfirmationChecker(config.options.confirmationInterval, config.identitySecret);
+});
